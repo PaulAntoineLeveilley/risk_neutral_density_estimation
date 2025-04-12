fixed issue with bakshi formula

pricing/bakshi_pricing.py
@@ -3,13 +3,27 @@ import math
 from scipy.integrate import quad
 
 
-def bakshi_prices(S: float, K: float, V: float, T: float, r: float, kappa: float, theta: float, lambd: float, rho: float, sigma: float, muj: float, sigmaj: float, upper_bound: float):
+def bakshi_prices(
+    S: float,
+    K: float,
+    V: float,
+    T: float,
+    r: float,
+    kappa: float,
+    theta: float,
+    lambd: float,
+    rho: float,
+    sigma: float,
+    muj: float,
+    sigmaj: float,
+    upper_bound: float,
+):
     """
     Computes the price of an european call option in the Bakshi model.
 
-    Parameters : 
+    Parameters :
     - S : spot price
-    - K : strike 
+    - K : strike
     - V : initial value for volatility
     - T : time to maturity
     - r : interest rate
@@ -22,16 +36,34 @@ def bakshi_prices(S: float, K: float, V: float, T: float, r: float, kappa: float
     - sigmaj : standard deviation of jump size
     - upper_bounb : upper bound for integral evaluation
     """
-    return S*P(1, S, T, V, K, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, upper_bound) - K*np.exp(-r*T)*P(2, S, T, V, K, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, upper_bound)
+    return S * P(
+        1, S, T, V, K, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, upper_bound
+    ) - K * np.exp(-r * T) * P(
+        2, S, T, V, K, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, upper_bound
+    )
 
 
-def rnd_bakshi(S: float, V: float, T: float, r: float, kappa: float, theta: float, lambd: float, rho: float, sigma: float, muj: float, sigmaj: float, upper_bound: float, x: float):
+def rnd_bakshi(
+    S: float,
+    V: float,
+    T: float,
+    r: float,
+    kappa: float,
+    theta: float,
+    lambd: float,
+    rho: float,
+    sigma: float,
+    muj: float,
+    sigmaj: float,
+    upper_bound: float,
+    x: float,
+):
     """
     Computes the risk neutral density of the Bakshi model
 
-    Parameters : 
+    Parameters :
     - S : spot price
-    - K : strike 
+    - K : strike
     - V : initial value for volatility
     - T : time to maturity
     - r : interest rate
@@ -43,46 +75,84 @@ def rnd_bakshi(S: float, V: float, T: float, r: float, kappa: float, theta: floa
     - muj : mean jump size
     - sigmaj : standard deviation of jump size
     - upper_bounb : upper bound for integral evaluation
-    - x : point at which the density is computed 
+    - x : point at which the density is computed
     """
-    def integrand(phi): return (np.exp(-1j*phi*np.log(x))*f(2, S, V,
-                                                            T, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, phi)).real
+
+    def integrand(phi):
+        return (
+            np.exp(-1j * phi * np.log(x))
+            * f(2, S, V, T, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, phi)
+        ).real
+
     integral = quad(integrand, 0, upper_bound)[0]
-    return integral/(math.pi*x)
+    return integral / (math.pi * x)
 
 
-def P(i: int, S: float, T: float, V: float, K: float, r: float, kappa: float, theta: float, lambd: float, rho: float, sigma: float, muj: float, sigmaj: float, upper_bound: float):
+def P(
+    i: int,
+    S: float,
+    T: float,
+    V: float,
+    K: float,
+    r: float,
+    kappa: float,
+    theta: float,
+    lambd: float,
+    rho: float,
+    sigma: float,
+    muj: float,
+    sigmaj: float,
+    upper_bound: float,
+):
     """
     Computes the i^th term in the Heston formula for a call option
 
     Parameters :
     - i : index of the term
-    - S : spot price 
-    - K : strike 
+    - S : spot price
+    - K : strike
     - T : time to maturity
     - r : interest rate
     - kappa : mean reversion parameter
     - theta : long run mean of volatity
     - lambd : jump rate
     - rho : correlation
-    - sigma : volatility of volatility     
+    - sigma : volatility of volatility
     - muj : mean jump size
     - sigmaj : standard deviation of jump size
     - upper_bound : upper bound for integral evaluation
     """
-    def integrand(phi): return (np.exp(-1j*phi*np.log(K))*f(i, S, V, T,
-                                                            r, kappa, theta, lambd, rho, sigma, muj, sigmaj, phi)/(1j*phi)).real
-    integral,error = quad(integrand, 0, upper_bound)
-    print("Estimate of the integration error :")
-    print(error)
-    return 0.5+integral/math.pi
+
+    def integrand(phi):
+        return (
+            np.exp(-1j * phi * np.log(K))
+            * f(i, S, V, T, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, phi)
+            / (1j * phi)
+        ).real
+
+    integral = quad(integrand, 0, upper_bound)[0]
+    return 0.5 + integral / math.pi
 
 
-def f(i: int, S: float, V: float, T: float, r: float, kappa: float, theta: float, lambd: float, rho: float, sigma: float, muj: float, sigmaj: float, phi: complex):
+def f(
+    i: int,
+    S: float,
+    V: float,
+    T: float,
+    r: float,
+    kappa: float,
+    theta: float,
+    lambd: float,
+    rho: float,
+    sigma: float,
+    muj: float,
+    sigmaj: float,
+    phi: complex,
+):
     """
     Computes term in the integrand for the complex integral.
 
-    Parameters : 
+    Parameters :
     - i : index of the term
     - S : Spot price
     - V : volatility
@@ -97,16 +167,27 @@ def f(i: int, S: float, V: float, T: float, r: float, kappa: float, theta: float
     - sigmaj : standard deviation of jump size
     - phi : integration variable
     """
-    C, D, E = compute_C_D_E(i, T, r, kappa, theta, lambd,
-                            rho, sigma, muj, sigmaj, phi)
-    return np.exp(C + D*V+E+1j*phi*np.log(S))
+    C, D, E = compute_C_D_E(i, T, r, kappa, theta, lambd, rho, sigma, muj, sigmaj, phi)
+    return np.exp(C + D * V + E + 1j * phi * np.log(S))
 
 
-def compute_C_D_E(i: int, T: float, r: float, kappa: float, theta: float, lambd: float, rho: float, sigma: float, muj: float, sigmaj: float, phi: complex):
+def compute_C_D_E(
+    i: int,
+    T: float,
+    r: float,
+    kappa: float,
+    theta: float,
+    lambd: float,
+    rho: float,
+    sigma: float,
+    muj: float,
+    sigmaj: float,
+    phi: complex,
+):
     """
     Computes the 'C' and the 'D' term in the expression of the function f
 
-    Parameters :  
+    Parameters :
     - i : index of the term
     - T : time to maturity
     - r : interest rate
@@ -119,19 +200,44 @@ def compute_C_D_E(i: int, T: float, r: float, kappa: float, theta: float, lambd:
     - sigmaj : standard deviation of jump size
     - phi : integration variable
     """
-    a = kappa*theta
+    a = kappa * theta
     b = kappa
     if i == 1:
-        b -= rho*sigma
+        b -= rho * sigma
         u = 0.5
     elif i == 2:
         u = -0.5
-    d = np.sqrt((1j*rho*sigma*phi-b)**2-(sigma**2)*(2j*u*phi-phi**2))
-    h = b-1j*rho*sigma*phi
-    g = (h+d)/(h-d)
-    C = 1j*r*phi*T+((h+d)*T-2*np.log((1-g*np.exp(d*T))/(1-g)))*a/sigma**2
-    D = (h+d)*(1-np.exp(d*T))/((1-g*np.exp(d*T))*(sigma**2))
-    E = lambd*(1+muj)*T*(((1+muj)**(1j*phi))*np.exp(1j*phi *
-                                                    (1+1j*phi)*(sigmaj**2)/2)-1)-1j*lambd*phi*muj*T
-    # E = lambd * T * (np.exp(1j * phi * muj - 0.5 * (sigmaj**2) * phi**2) - 1)
+    d = np.sqrt(
+        (1j * rho * sigma * phi - b) ** 2 - (sigma**2) * (2j * u * phi - phi**2)
+    )
+    h = b - 1j * rho * sigma * phi
+    g = (h + d) / (h - d)
+    C = (
+        1j * r * phi * T
+        + ((h + d) * T - 2 * np.log((1 - g * np.exp(d * T)) / (1 - g))) * a / sigma**2
+    )
+    D = (h + d) * (1 - np.exp(d * T)) / ((1 - g * np.exp(d * T)) * (sigma**2))
+    if i == 1:
+        E = (
+            lambd
+            * (1 + muj)
+            * T
+            * (
+                ((1 + muj) ** (1j * phi))
+                * np.exp(1j * phi * (1 + 1j * phi) * (sigmaj**2) / 2)
+                - 1
+            )
+            - 1j * lambd * phi * muj * T
+        )
+    elif i == 2:
+        E = (
+            lambd
+            * T
+            * (
+                ((1 + muj) ** (1j * phi))
+                * np.exp(1j * phi * (1j * phi - 1) * (sigmaj**2) / 2)
+                - 1
+            )
+            - 1j * lambd * phi * muj * T
+        )
     return C, D, E