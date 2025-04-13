52e97dd27ad0aa7ee3097c13866d295b08c404d7
Paul-Antoine Leveilley <paul-antoine.leveilley@dauphine.eu>
2025-04-13 09:42:41 +0200

    integrated kernel regression into the code

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from data_generating_process import generate_call_prices, compute_implied_volatility
+from interpolation.kernel_regression import interpolating_kernelreg
+from interpolation.cubic_splines import interpolating_cs
 from interpolation.rbf_neural_network import interpolating_rbf
 from config import (
     S_OVER_K_RANGE,
@@ -49,7 +51,7 @@ def main():
         "lambd": lambd,
     }
     data = generate_call_prices(
-        T, maturity, model, model_parameters, 1, compute_vega, 100, upper_bound
+        T, maturity, model, model_parameters, 2, compute_vega, 100, upper_bound
     )
 
     spot_prices = data["spot_prices"]
@@ -60,37 +62,25 @@ def main():
     )
     implied_vol_reversed = implied_volatility[:,::-1]
 
-
-    interpolation_rbf = interpolating_rbf(S_OVER_K_RANGE,implied_vol_reversed,num_centers=5)
-
-    
-    
-    X_train = np.array(S_OVER_K_RANGE).reshape(-1, 1)
+    X_train = np.array(S_OVER_K_RANGE)
     y_train = implied_vol_reversed[0]
-    plt.figure(figsize=(10, 5))
-    plt.plot(X_train, y_train, "o", label="Implied volatility (target)")
-    plt.xlabel("S/K")
-    plt.ylabel("Implied volatility")
-    plt.title("Target data for training the RBF network")
-    plt.legend()
-    plt.grid(True)
-    plt.show()
-    
-    X_test = np.linspace(1/RELATIVE_STRIKE_UPPER_BOUND, 1/RELATIVE_STRIKE_LOWER_BOUND, 200).reshape(-1, 1)
-    y_pred = interpolation_rbf[0](X_test)
+
+    estimator = interpolating_kernelreg(S_OVER_K_RANGE,implied_vol_reversed)[0]
+    strike_range = np.linspace(
+    RELATIVE_STRIKE_LOWER_BOUND, RELATIVE_STRIKE_UPPER_BOUND, 500
+    )
+    coarse_s_over_k_range = np.array([1 / K for K in strike_range[::-1]])
+    interpolating_line = estimator(coarse_s_over_k_range)
     
     plt.figure(figsize=(10, 5))
     plt.plot(X_train, y_train, "o", label="Target implied volatility")
-    plt.plot(X_test, y_pred, "-", label="RBF network estimation")
+    plt.plot(coarse_s_over_k_range, interpolating_line, "-", label="kernel regression estimation")
     plt.xlabel("S/K")
     plt.ylabel("Implied volatility")
-    plt.title("RBF Network Estimation of Implied Volatility")
+    plt.title("kernel regression Estimation of Implied Volatility")
     plt.legend()
     plt.grid(True)
     plt.show()
     
-    
-
-
 if __name__ == "__main__":
     main()
