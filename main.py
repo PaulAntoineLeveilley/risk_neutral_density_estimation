0926d10c4941b28392d3270ce93e4812eaeb0598
Paul-Antoine Leveilley <paul-antoine.leveilley@stackease.ai>
2025-04-17 20:26:00 +0200

    changed upper bound in config file to 200

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from full_estimating_procedure import full_estimating_procedure
 import time
 def main():
-    start = time.time()
     T = 1
     maturities = [21/252,42/252,63/252,84/252,105/252,126/252]
     # model = 'black_scholes'
     # interpolation_method = 'cubic_splines'
 
-    models = ['black_scholes','heston',"bakshi"]
+    models = ['heston']
     interpolation_methods = ["cubic_splines", 'kernel_regression','rbf_network']
     for model in models:
         for interpolation_method in interpolation_methods:
